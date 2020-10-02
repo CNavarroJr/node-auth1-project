cf6fddc9846ebac5c63ef04fc8cc4eed77209b1e
Junior <junior@Juniors-Mac-mini.local>
2020-10-02 06:56:39 -0500

    Added all neccessary files

---
+++ api/server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+// const errorHandler = require("./errorHandler.js");
+const session = require("express-session");
+const KnexSessionStore = require("connect-session-knex");
+
+
+const userRouter = require("./users/users-router.js");
+const authRouter = require("./auth/auth-router.js");
+
+const server = express();
+
+const sessionConfig = {
+  name: "monkey",
+  secret: "I can't tell you because its a secret",
+  cookie: {
+    maxAge: 60 * 60 * 1000,
+    secure: false,
+    httpOnly: true
+  },
+  resave: false,
+  saveUnitialized: false,
+
+
+  store: new KnexSessionStore({
+    knex: require("../database/connection.js"),
+    tablename: 'sessions',
+    sidfieldname: 'sid',
+    createtable: true,
+    clearInterval: 60 * 60 * 1000
+  })
+}
+
+
+server.use(session(sessionConfig));
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+server.use("/api/users", userRouter);
+server.use("/api/auth", authRouter);
+
+server.get("/", (req, res) => {
+  res.json({ api: "up"});
+});
+
+server.use(errorHandler);
+
+module.exports = server;
---
+++ users/users-router.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const restricted = require('../auth/restricted-middleware.js');
+
+const Users = require("./users-model.js");
+
+router.get('/', (req, res) => {
+
+  Users.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => res.send(err));
+});
+
+module.exports = router;
